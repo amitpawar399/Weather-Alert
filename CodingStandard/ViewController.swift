ebf597468798f4bc609a3ef0a90dfa58218c6de0
Amit Pawar <amit.pawar@docplexus.in>
2017-11-02 10:33:03 +0530

    Initial Commit

---
+++ CodingStandard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodingStandard
+//
+//  Created by Rushikesh Talokar on 02/11/17.
+//  Copyright © 2017 Rushikesh Talokar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
